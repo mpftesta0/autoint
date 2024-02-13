272e92a1b43cba48b7320d19ba38af64f3ae1d17
Miguel Testa <mpftesta@gmail.com>
2024-02-12 19:30:14 -0800

    feat: added assistant updating functionality

--- README.md
+++ README.md
@@ -28,55 +28,49 @@ Run `bathala` to start the server. It will take requests through http://localhos
 
 1. Define your OpenAI Assistant parameters in `assistants/`
 
-```typescript
-// assistants/demoAssistant.ts
-import OpenAI from "openai";
-
-const demoAssistant: OpenAI.Beta.AssistantUpdateParams = {
-  name: "Demo Assistant",
-  instructions:
-    "You are a weather bot. Use the provided functions to answer questions.",
-  model: "gpt-4-turbo-preview",
-  tools: [
+```json
+{
+  "name": "Demo Assistant",
+  "instructions": "You are a weather bot. Use the provided functions to answer questions.",
+  "model": "gpt-4-turbo-preview",
+  "tools": [
     {
-      type: "function",
-      function: {
-        name: "getCurrentWeather",
-        description: "Get the weather in location",
-        parameters: {
-          type: "object",
-          properties: {
-            location: {
-              type: "string",
-              description: "The city and state e.g. San Francisco, CA",
+      "type": "function",
+      "function": {
+        "name": "getCurrentWeather",
+        "description": "Get the weather in location",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "location": {
+              "type": "string",
+              "description": "The city and state e.g. San Francisco, CA"
             },
-            unit: { type: "string", enum: ["c", "f"] },
+            "unit": { "type": "string", "enum": ["c", "f"] }
           },
-          required: ["location"],
-        },
-      },
+          "required": ["location"]
+        }
+      }
     },
     {
-      type: "function",
-      function: {
-        name: "getNickname",
-        description: "Get the nickname of a city",
-        parameters: {
-          type: "object",
-          properties: {
-            location: {
-              type: "string",
-              description: "The city and state e.g. San Francisco, CA",
-            },
+      "type": "function",
+      "function": {
+        "name": "getNickname",
+        "description": "Get the nickname of a city",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "location": {
+              "type": "string",
+              "description": "The city and state e.g. San Francisco, CA"
+            }
           },
-          required: ["location"],
-        },
-      },
-    },
-  ],
-};
-
-export default demoAssistant;
+          "required": ["location"]
+        }
+      }
+    }
+  ]
+}
 ```
 
 2. Define function tools in `tools/functions`. Each function file must be named EXACTLY the same as the function tool as defined in your assistants file.
---
+++ scripts/updateAssistants.ts
@@ -0,0 +1,129 @@
+import { promises as fs } from "fs";
+import * as path from "path";
+import OpenAI from "openai";
+
+const __dirname = process.cwd();
+
+const openai = new OpenAI();
+
+interface Assistants {
+  [assistantName: string]: string;
+}
+
+async function doesFolderExist(folderPath: string) {
+  try {
+    await fs.access(folderPath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function doesFileExist(filePath: string) {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function createFolder(folderPath: string) {
+  try {
+    await fs.mkdir(folderPath, { recursive: true });
+    console.log("Folder created:", folderPath);
+  } catch (err) {
+    console.error("Error creating folder:", err);
+  }
+}
+
+async function readAssistantsFileToJson(filePath: string): Promise<Assistants> {
+  try {
+    const fileContent = await fs.readFile(filePath, "utf8");
+    const jsonData = JSON.parse(fileContent);
+    console.log("JSON Content:", jsonData);
+    return jsonData;
+  } catch (err) {
+    console.error("Error reading file:", err);
+    throw new Error("Error reading file");
+  }
+}
+
+async function main() {
+  const bathalaFolderPath = path.join(__dirname, ".bathala");
+  const assistantIdsFilePath = path.join(bathalaFolderPath, "assistants.json");
+  const assistantsFolderPath = path.join(__dirname, "assistants");
+
+  const doesBathalaFolderExist = await doesFolderExist(bathalaFolderPath);
+
+  if (doesBathalaFolderExist) {
+    const doesAssistantsFileExist = await doesFileExist(assistantIdsFilePath);
+
+    if (doesAssistantsFileExist) {
+      const assistantsInFile = await readAssistantsFileToJson(
+        assistantIdsFilePath
+      );
+      const assistantsInFolder = await fs.readdir(assistantsFolderPath);
+
+      for (const assistantIdx in assistantsInFolder) {
+        const assistant = assistantsInFolder[assistantIdx];
+        const assistantFilePath = path.join(assistantsFolderPath, assistant);
+        const assistantFileContent = await fs.readFile(
+          assistantFilePath,
+          "utf8"
+        );
+        const assistantJson = JSON.parse(assistantFileContent);
+        const assistantId = assistantsInFile[assistant];
+
+        if (assistantId) {
+          console.log("Updating assistant:", assistant);
+          const updateAssistantResponse = await openai.beta.assistants.update(
+            assistantId,
+            assistantJson
+          );
+          console.log("Assistant updated:", updateAssistantResponse);
+        } else {
+          console.log("Creating assistant:", assistant);
+          const createAssistantResponse = await openai.beta.assistants.create(
+            assistantJson
+          );
+          console.log("Assistant created:", createAssistantResponse);
+          assistantsInFile[assistant] = createAssistantResponse.id;
+        }
+
+        await fs.writeFile(
+          assistantIdsFilePath,
+          JSON.stringify(assistantsInFile, null, 2)
+        );
+      }
+    } else {
+      const assistantsInFolder = await fs.readdir(assistantsFolderPath);
+      const createdAssistants = {};
+
+      for (const assistantIdx in assistantsInFolder) {
+        const assistant = assistantsInFolder[assistantIdx];
+        const assistantFilePath = path.join(assistantsFolderPath, assistant);
+        const assistantFileContent = await fs.readFile(
+          assistantFilePath,
+          "utf8"
+        );
+        const assistantJson = JSON.parse(assistantFileContent);
+        const createAssistantResponse = await openai.beta.assistants.create(
+          assistantJson
+        );
+        console.log("Assistant created:", createAssistantResponse);
+        createdAssistants[assistant] = createAssistantResponse.id;
+      }
+
+      await fs.writeFile(
+        assistantIdsFilePath,
+        JSON.stringify(createdAssistants, null, 2)
+      );
+    }
+  } else {
+    await createFolder(bathalaFolderPath);
+    await main();
+  }
+}
+
+await main();
